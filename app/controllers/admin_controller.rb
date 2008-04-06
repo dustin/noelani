6260ecbc277a875100bd4db49fc3ad6c808fb8d5
Dustin Sallings <dustin@spy.net>
2008-04-05 20:53:05 -0700

    Basic admin for blog.

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,27 @@
+class AdminController < ApplicationController
+  include AuthenticatedSystem
+
+  before_filter :login_required
+
+  def index
+    # show a template
+  end
+
+  def blog
+    @posts = BlogPost.find :all, :order => "created_at desc"
+  end
+
+  def edit_post
+    @blog_post = BlogPost.find params[:id] if params[:id]
+    if request.post?
+      # Save and stuff
+      if @blog_post
+        @blog_post.update_attributes! params[:blog_post]
+      else
+        p=params[:blog_post].merge({:author_id => current_user.id})
+        BlogPost.create! p
+      end
+      redirect_to :action => :blog
+    end
+  end
+end
