08cf95f21e6d0dcacbcc91ca85135f6b7e0ce11f
Dustin Sallings <dustin@spy.net>
2008-04-19 15:20:09 -0700

    Endorsement controller uses the endorsement model now.

--- app/controllers/endorsements_controller.rb
+++ app/controllers/endorsements_controller.rb
@@ -2,6 +2,8 @@ class EndorsementsController < ApplicationController
   
   def index
     subtitle 'Endorsements'
+    @electeds = EndorsementList.find_by_title 'Elected Officials'
+    @leaders = EndorsementList.find_by_title 'Community Leaders'
   end
 
 end
