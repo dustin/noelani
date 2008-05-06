9bee633d81a049262a61a7b303458ee507ed76e6
Dustin Sallings <dustin@spy.net>
2008-05-05 23:17:41 -0700

    Fix the quotes on the home page with a new helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def q_sans_whitespace(&block)
+    "<q>#{capture_haml(&block).strip}</q>"
+  end
+
 end
