cb32a1c78af40074e1dd026065c5b28f4aa08587
Dustin Sallings <dustin@spy.net>
2008-05-05 23:51:50 -0700

    Converted the endorsements page to haml.

--- app/helpers/endorsements_helper.rb
+++ app/helpers/endorsements_helper.rb
@@ -1,2 +1,11 @@
 module EndorsementsHelper
+
+  def sup_dagger
+    "<sup>&dagger;</sup>"
+  end
+
+  def show_endorser_title(title)
+    title.strip.split(/\r?\n/).join("#{sup_dagger}<br/>") + sup_dagger
+  end
+
 end
