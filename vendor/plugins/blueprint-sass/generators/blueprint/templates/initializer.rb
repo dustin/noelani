98c3aa6ba264f306a75ff5bd5975df1cede33a69
Dustin Sallings <dustin@spy.net>
2008-08-19 21:52:37 -0700

    Added blueprint plugin.

---
+++ vendor/plugins/blueprint-sass/generators/blueprint/templates/initializer.rb
@@ -0,0 +1,5 @@
+Sass::Plugin.options[:template_location] = {
+  "#{RAILS_ROOT}/app/stylesheets" => "#{RAILS_ROOT}/public/stylesheets",
+  "#{RAILS_ROOT}/vendor/plugins/blueprint-sass/blueprint" => "#{RAILS_ROOT}/public/stylesheets/blueprint"
+}
+
