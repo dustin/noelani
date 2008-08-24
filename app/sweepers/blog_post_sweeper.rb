19786b9def3e54806730e2a1a9bb5abb73086576
Dustin Sallings <dustin@spy.net>
2008-08-24 00:58:20 -0700

    Qualify the controller in the sweeper cache expirer.

--- app/sweepers/blog_post_sweeper.rb
+++ app/sweepers/blog_post_sweeper.rb
@@ -10,8 +10,8 @@ class BlogPostSweeper < ActionController::Caching::Sweeper
   end
 
   def expire_cache(blog_post)
-    expire_page :controller => 'blog', :action => :index
-    expire_page :controller => 'admin/blog', :action => :index
+    expire_page :controller => '/blog', :action => :index
+    expire_page :controller => '/admin/blog', :action => :index
   end
 
 end
